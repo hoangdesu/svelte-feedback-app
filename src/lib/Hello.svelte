fd8fd20e123fc817f534c1dd898dcd8843a95eda
Hoang Nguyen <hoangdesu@gmail.com>
2022-11-04 20:20:28 +0700

    reactive value

---
+++ src/lib/Hello.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let name;
+
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+
+    const formatName = (name) => {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+
+    const formattedName = formatName(name);
+</script>
+
+<div>
+    <h1>Hello {formattedName}! ^o^</h1>
+</div>
---
+++ src/lib/MessingAround.svelte
@@ -0,0 +1,11 @@
+<script>
+    let firstName = 'Brian';
+    let lastName = 'Nguyen';
+    let color = 'yellow';
+
+    $: name = firstName + ' ' + lastName;
+</script>
+
+<main>
+    <h1 style="color: {color}">Hello {name}!</h1>
+</main>
