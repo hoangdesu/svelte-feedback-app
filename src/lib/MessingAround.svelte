e80bc67eb496819f2df1862733a6223b050d6796
Hoang Nguyen <hoangdesu@gmail.com>
2022-11-04 20:42:44 +0700

    conditional rendering

--- src/lib/MessingAround.svelte
+++ src/lib/MessingAround.svelte
@@ -3,9 +3,33 @@
     let lastName = 'Nguyen';
     let color = 'yellow';
 
+    // $: reactive value
+    // mark a statement as reactive 
+    // must be on top-level
+    // run after other script code, before markup is rendered, whenever the values they depend on have changed
     $: name = firstName + ' ' + lastName;
+
+    const toggle = () => {
+        color = color === 'yellow' ? 'pink' : 'yellow';
+    };
+
+    // conditional
+    $: showText = false;
+
 </script>
 
-<main>
+<div>
     <h1 style="color: {color}">Hello {name}!</h1>
-</main>
+
+    <button on:click={toggle}>Toggle color</button>
+
+    <button on:click={() => {showText = !showText}}>Show text</button>
+
+    <!-- conditional rendering -->
+    {#if showText}
+        <p> Ú oà!</p>
+    {:else}
+        <p>;)</p>
+    {/if}
+
+</div>
